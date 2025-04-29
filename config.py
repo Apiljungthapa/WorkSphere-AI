3f2d5cfc906238f2ecaeedb8b6cacd9af1f056a6
Apil_Thapa <137038201+Apiljungthapa@users.noreply.github.com>
2025-04-29 11:14:53 +0545

    removed file

--- config.py
+++
@@ -1,3 +0,0 @@
-from imports import *
-
-AI_BASE_DIR = os.path.dirname(os.path.abspath(__file__))
--- utils/state.py
+++
@@ -1,7 +0,0 @@
-from imports import *
-
-class ContentState(MessagesState):
-   Introduction : dict
-   Body : dict
-   Conclusion: dict
-   FinalResult :dict
